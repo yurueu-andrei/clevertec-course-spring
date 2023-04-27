660920aada48fd3243acbd74f646635cb78ddab3
Dobrowydka <andrikchannelyou@gmail.com>
2023-04-27 23:48:43 +0300

    feat: exceptions

---
+++ src/main/java/ru/clevertec/ecl/exception/RepositoryException.java
@@ -0,0 +1,7 @@
+package ru.clevertec.ecl.exception;
+
+public class RepositoryException extends RuntimeException {
+    public RepositoryException(String message) {
+        super(message);
+    }
+}
