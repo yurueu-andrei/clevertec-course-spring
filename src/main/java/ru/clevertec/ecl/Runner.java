8e44738cb898506f0742ea1b76559815fc1c5452
Andrei <andrei.yurueu1@gmail.com>
2023-05-10 18:51:14 +0300

    feat: runner class

---
+++ src/main/java/ru/clevertec/ecl/Runner.java
@@ -0,0 +1,11 @@
+package ru.clevertec.ecl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Runner {
+    public static void main(String[] args) {
+        SpringApplication.run(Runner.class, args);
+    }
+}
